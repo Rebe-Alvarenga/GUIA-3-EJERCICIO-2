b9392c435e2ed663daa6604c6468f3d7a3c52544
Rebe-Alvarenga <rebequitasarai1@gmail.com>
2021-01-29 21:49:04 -0600

    Final Upload

--- GUIA 3 EJERCICIO 2/Program.cs
+++ GUIA 3 EJERCICIO 2/Program.cs
@@ -6,7 +6,23 @@ namespace GUIA_3_EJERCICIO_2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            /*
+             *PROGRAMACION ESTRUCTURADA G07L
+             *REBECA SARAÍ ALVARENGA HERNÁNDEZ
+             *
+             *EJERCICIO #2 GUIA 3
+             */
+
+            double b, h, a;
+            Console.WriteLine("Hola y bienvenido :) Te solicitare solamente dos datos, la base y la altura.");
+            Console.WriteLine("Empecemos con la base, ¿cual es la medida de la base?");
+            b = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Bien, ahora, ¿ cual es la medida de la altura?");
+            h = Convert.ToDouble(Console.ReadLine());
+            a = (b * h) / 2;
+            Console.WriteLine("\nEl area del triangulo propuesto es de " + a);
+            Console.WriteLine("\nPresione cualquier tecla para finalizar el programa...");
+            Console.ReadKey();
         }
     }
 }
